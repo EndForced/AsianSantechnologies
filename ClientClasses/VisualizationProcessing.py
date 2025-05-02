34f611adc369d0ecff1a01929d882c7e262eceb4
EndForced <evafagsclub@gmail.com>
2025-05-03 00:06:23 +1000

    волновой алгоритм + отображение

--- ClientClasses/VisualizationProcessing.py
+++ ClientClasses/VisualizationProcessing.py
@@ -1,29 +1,30 @@
 import cv2
 import numpy as np
 
-
-class VisualizeMatrix:
+import wayProcessingOperations.BasicWaveOperations
+from wayProcessingOperations.BasicWaveOperations import possible_codes
+class VisualizeMatrix(wayProcessingOperations.BasicWaveOperations.WaveCreator):
     import cv2
-    from typing import List
+    from typing import List, AnyStr
     import numpy as np
 
     def __init__(self, matrix:list[list[int]]):
-        if np.array(matrix).ndim != 2:
-            raise ValueError("Matrix dimension error!11!!1! mat:", matrix)
-
+        super().__init__(matrix)
         self.pathToPics = "field_pictures/"
-        self._possibleCodes = [0, 10, 20, 31,32,33,34, 41,42, 51,52, 61,62,63,64, 71,72,73,74]
+        self._possibleCodes = possible_codes
         #ждём текстуры для 81,82,83,84(робот на втором этаже) 91,92,93,94 - (робот на рампах)
         self._matrix = matrix
 
-        self._matrixMaxHeight, self._matrixMaxWeight = self._matrix_max_dimensions()
-        self._picture = self._image_by_matrix_size()
+        self._matrixMaxHeight, self._matrixMaxWeight = self.matrix_max_dimensions(self._matrix)
+        self.picture = self.image_by_matrix_size()
 
-        self._codesInMatrix = self._find_included_codes()
-        self._cached_images =  self._cache_images()
+        self._codesInMatrix = self.find_included_codes()
+        self._cached_images =  self.cache_images()
         self.visualize_matrix()
 
-    def _find_included_codes(self):
+        self.resizedPicture = self.smart_resize()
+
+    def find_included_codes(self):
         codes = []
         for i in range(len(self._matrix)):
             for j in range(len(self._matrix[i])):
@@ -36,7 +37,7 @@ class VisualizeMatrix:
                     raise ValueError("Impossible code found:",code)
         return codes
 
-    def _image_by_matrix_size(self):
+    def image_by_matrix_size(self):
         #создает картинку согласно максимальным измерениям матрицы
 
         if self._matrixMaxHeight > 0 and self._matrixMaxWeight > 0:
@@ -45,13 +46,8 @@ class VisualizeMatrix:
 
         return picture
 
-    def _matrix_max_dimensions(self):
-        #возвращает максимальные измерения матрицы переданной в класс в виде
-        #[макс высота, макс ширина]
 
-        return len(self._matrix), max(len(row) for row in self._matrix) if len(self._matrix) > 0 else 0
-
-    def _cache_images(self):
+    def cache_images(self):
         pic_dictionary = {}
         for i in self._codesInMatrix:
             pic_dictionary[i] = np.uint16(cv2.imread(f"{self.pathToPics}{i}.png") * (65535 / 255))
@@ -59,7 +55,7 @@ class VisualizeMatrix:
         return pic_dictionary
 
     def visualize_matrix(self):
-        print(self._picture.shape)
+        # print(self.picture.shape)
         for i in range(self._matrixMaxHeight):
             for j in range(len(self._matrix[i])):
                 code = self._matrix[i][j]
@@ -67,34 +63,23 @@ class VisualizeMatrix:
                 x = j*100, (j+1)*100
                 # print(self._picture.dtype)
                 # print(self._cached_images[code].dtype)
-                self._picture[y[0]:y[1],x[0]:x[1]] = self._cached_images[code]
+                self.picture[y[0]:y[1],x[0]:x[1]] = self._cached_images[code]
 
-        if max(self._picture.shape) > 600:
-            return self._smart_resize()
+        if max(self.picture.shape) > 600:
+            return self.smart_resize()
 
-        return self._picture
+        return self.picture
 
-    def _smart_resize(self, target_size=600, keep_aspect_ratio=True, interpolation=cv2.INTER_AREA):
-        """
-        Улучшенный ресайз изображения с автоматическим подбором коэффициента масштабирования
-        и сохранением качества.
+    def smart_resize(self, target_size=600, keep_aspect_ratio=True, interpolation=cv2.INTER_AREA):
 
-        Параметры:
-            target_size (int): максимальный размер по любой из сторон (по умолчанию 600)
-            keep_aspect_ratio (bool): сохранять соотношение сторон (True по умолчанию)
-            interpolation: метод интерполяции (по умолчанию INTER_AREA для уменьшения)
-
-        Возвращает:
-            None (изменяет self._picture напрямую)
-        """
-        if self._picture is None:
+        if self.picture is None:
             raise ValueError("Input image is None")
 
-        h, w = self._picture.shape[:2]
+        h, w = self.picture.shape[:2]
 
         # Если изображение уже меньше целевого размера, не изменяем его
         if h <= target_size and w <= target_size:
-            return
+            return self.picture
 
         # Ваш оригинальный подход с автоматическим расчетом коэффициента
         max_dim = max(h, w)
@@ -110,30 +95,157 @@ class VisualizeMatrix:
 
         # Применяем ресайз с выбранной интерполяцией
         resized = picture = cv2.resize(
-            self._picture,
+            self.picture,
             (new_w, new_h),
             interpolation=interpolation
         )
 
         # Опционально: добавляем логгирование
-        print(f"Resized from {w}x{h} to {new_w}x{new_h} (scale factor: {scale_factor:.2f})")
+        # print(f"Resized from {w}x{h} to {new_w}x{new_h} (scale factor: {scale_factor:.2f})")
         return resized
 
+    def show(self):
+        self.resizedPicture = self.smart_resize()
+        cv2.imshow("map", self.resizedPicture)
+
+        cv2.waitKey(0)
+
+
+class VisualizeWaves(VisualizeMatrix):
+
+    def __init__(self, matrix:list[list[int]]):
+        super().__init__(matrix)
+        self.frameWeight = 5
+        self.fontSize = 0.7
+        self.fontThickness = 2
+        self.maxWavesNum = 0
+
+    def put_frame(self, cords: tuple[int, int], base_color: tuple[int, int, int]):
+        """Рисует рамку вокруг клетки с автоматическим подбором контрастного цвета.
+
+        Args:
+            cords: Координаты клетки (y, x)
+            base_color: Базовый цвет рамки в формате (B, G, R) [0-65535]
+        """
+        # Получаем область клетки
+        y_start, y_end = cords[0] * 100, (cords[0] + 1) * 100
+        x_start, x_end = cords[1] * 100, (cords[1] + 1) * 100
+
+        # Корректировка границ для крайних клеток
+        if cords[0] == 0: y_start += 5
+        if cords[1] == 0: x_start += 5
+
+        # Получаем средний цвет клетки (BGR)
+        cell_area = self.picture[y_start:y_end, x_start:x_end]
+        mean_color = np.mean(cell_area, axis=(0, 1))
+
+        # Вычисляем яркость (формула восприятия)
+        brightness = 0.299 * mean_color[2] + 0.587 * mean_color[1] + 0.114 * mean_color[0]
+
+        # Преобразуем базовый цвет в numpy array для операций
+        base_color_np = np.array(base_color, dtype=np.float32)
+
+        # Выбираем контрастный цвет
+        if brightness > 32768:  # Светлая клетка
+            frame_color = base_color_np * 0.7  # Темнее
+        else:  # Темная клетка
+            frame_color = base_color_np * 1.3  # Светлее
+
+        # Ограничиваем диапазон и преобразуем
+        frame_color = np.clip(frame_color, 0, 65535).astype(np.uint16)
+
+        # Рисуем рамку
+        weight = self.frameWeight
+        self.picture[y_start - weight:y_start, x_start:x_end] = frame_color  # Верх
+        self.picture[y_end - weight:y_end, x_start:x_end] = frame_color  # Низ
+        self.picture[y_start:y_end, x_start - weight:x_start] = frame_color  # Лево
+        self.picture[y_start:y_end, x_end - weight:x_end] = frame_color  # Право
+
+    def put_text(self, cords: tuple, text: str, color=(65535/2, 65535/2, 65535/2)):
+        #Максимум - 3 символа !!!11!!11!
+        if type(text) != "str":
+            text = str(text)
+
+        if cords[0] >= len(self._matrix) or cords[1] >= len(self._matrix[cords[0]]):
+            raise ValueError("Trying to display text out of matrix range:", cords, "matrix:", self._matrix)
+        temp_img = np.zeros(self.picture.shape[:2], dtype=np.uint8)
+
+        x = int((cords[1] + 1) * 100 - 99 + 5 * abs(3 - len(text)))
+        y = int(cords[0] * 100 + 84)
+
+        cv2.putText(
+            temp_img,
+            text,
+            org=(x, y),
+            fontScale=self.fontSize,
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            color=255,
+            thickness=self.fontThickness,
+            lineType=cv2.LINE_8
+        )
+
+        mask = temp_img > 0
+
+        for c in range(3):
+            self.picture[:, :, c][mask] = color[c]
+
+    def visualize_wave(self,waves:dict = None):
+        waves = waves or self.Waves
+        self.maxWavesNum = len(waves)
+        for wave in waves:
+            for cell in wave:
+                current_number = waves.index(wave)
+                color_temp = self.scale_value_waves(current_number)
+                color = self._interpolate_color(color_temp)
+                self.put_text(cell,current_number)
+                self.put_frame(cell,color)
+
+    @staticmethod
+    def _interpolate_color(t):
+        if (t < 1 or t == 1) and (t > 0 or t == 0):
+            # переводит значение от нуля до единицы в цвет от красного до синего, думаю мне не нужно это объяснять
+            light_blue = (75, 120, 230)
+            bright_red = (255, 0, 0)
+            t = t - 0.05
+            red = int(light_blue[0] + (bright_red[0] - light_blue[0]) * t)
+            green = 0
+            blue = int(light_blue[2] + (bright_red[2] - light_blue[2]) * t)
+
+            return blue*257, green*257, red*257
+        else:
+            print("Fail during colour interpolation! arg can't be > 1 or < 0 !  \n arg:", t)
+            return None
+
+    def scale_value_waves(self,value):
+        from_min, to_min = 0, 0
+        from_max = self.maxWavesNum
+        to_max = 1
+
+        normalized_value = (value - from_min) / (from_max - from_min)  # map() из ардуино но в питоне
+
+        scaled_value = to_min + normalized_value * (to_max - to_min)
+
+        return scaled_value
+
+if __name__ == "__main__":
+    # mat = [[10]*2]*2
+    mat = [[10, 32, 20, 20, 20, 34, 10, 31],
+           [10, 32, 20, 10, 32, 20, 20, 33],
+           [10, 20, 20, 71, 10, 10, 20, 10],
+           [10, 20, 20, 34, 10, 10, 31, 10],
+           [10, 10, 20, 20, 20, 34, 10, 62],
+           [20, 10, 10, 10, 10, 10, 10, 62],
+           [10, 10, 10, 20, 10, 10, 10, 62],
+           [10, 41, 20, 20, 20, 34, 41, 10]]
+
+    obj = VisualizeWaves(mat)
+    obj.create_wave((2,3))
+    print(obj.Waves)
+
+    obj.visualize_wave()
+    obj.show()
 
 
 
-mat = [[10]*15]*15
-       # print(mat)
-
-c = VisualizeMatrix(mat)
-pic = c.visualize_matrix()
-# print(pic)
-cv2.imshow("pic",pic)
-cv2.waitKey(0)
-c.__init__(mat)
-
-cv2.imshow("pic",c._picture)
-cv2.waitKey(0)
-#tsttst
 
 
---
+++ wayProcessingOperations/BasicWaveOperations.py
@@ -0,0 +1,137 @@
+from typing import List
+
+import numpy as np
+from numpy.matrixlib.defmatrix import matrix
+import time
+
+possible_codes = [0, 10, 20, 31,32,33,34, 41,42, 51,52, 61,62,63,64, 71,72,73,74]
+
+class WaveCreator:
+    def __init__(self,matrix_in:list[list[int]]):
+        self._matrix = matrix_in
+
+        if not self._matrix or max(self.matrix_max_dimensions(self._matrix)) == 1:
+            raise ValueError("Cant create neighbor dictionary with this matrix:" , self._matrix)
+
+        self.Waves = []
+
+        self.cellsConnections = {10:{"up":[31,10], "left":[34,10], "down":[33,10], "right":[32,10]},
+                                 20:{"up":[33,20], "left":[32,20], "down":[31,20], "right":[34,20]},
+
+                                 31:{"up":[33,20], "left":[None], "down":[33,10], "right":[None]},
+                                 32:{"up":[None], "left":[10,34], "down":[None], "right":[20]},
+                                 33:{"up":[31], "left":[None], "down":[31,20], "right":[None]},
+                                 34:{"up":[None], "left":[20,32], "down":[None], "right":[10]},
+
+                                 71:{"up":[31,10], "left":[34,10], "down":[33,10], "right":[32,10]},
+                                 72: {"up": [31,10], "left": [34,10], "down": [33,10], "right": [32,10]},
+                                 73: {"up": [31,10], "left": [34,10], "down": [33,10], "right": [32,10]},
+                                 74: {"up": [31,10], "left": [34,10], "down": [33,10], "right": [32,10]},
+
+                                 61: {"up": [None], "left": [None], "down": [None], "right": [None]},
+                                 62: {"up": [None], "left": [None], "down": [None], "right": [None]},
+                                 63: {"up": [None], "left": [None], "down": [None], "right": [None]},
+                                 64: {"up": [None], "left": [None], "down": [None], "right": [None]},
+
+                                 41: {"up": [None], "left": [None], "down": [None], "right": [None]},
+                                 42: {"up": [None], "left": [None], "down": [None], "right": [None]},
+
+                                 51: {"up": [None], "left": [None], "down": [None], "right": [None]},
+                                 52: {"up": [None], "left": [None], "down": [None], "right": [None]},
+                                 }
+
+        self._matrixConnections = self._process_cells_connections()
+
+    def _process_cells_connections(self):
+        #возвращает список вида [ {(y1,x1):[(y2,x2),(y3,x3)]}, ...]
+        #где y1 и x1 - координаты клетки, а y2,x2,y3,x3 - координаты клеток в которые можно из нее пройти
+
+        connections_dict = {}
+        for i in range(len(self._matrix)):
+            for j in range(len(self._matrix[i])):
+
+                res = self.get_relative_cells((i,j))
+                res = self._find_valid_connections(res[0], res[1])
+                connections_dict[res[1]] = res[0]
+
+        return connections_dict
+
+    def _find_valid_connections(self, cell_dict, cell):
+        #принимает словарь вида {"up":[], "right":[(0,1)], "down":[(1,0)], "left":[]} и координаты клетки вида (y,x)
+        #исключает из исходного словаря те клетки в которые нельзя пройти по правилам в cellsConnections
+        #перед return убирает направления т.к они больше не нужны
+        #пример return [[(1, 0)], [(0, 1)]], (0, 0)
+
+        mat_tmp = np.array(self._matrix)
+        valid_cells = []
+
+        if mat_tmp[cell] not in possible_codes:
+            raise ValueError("Unknown cell while wave constructing:", mat_tmp[cell])
+
+        for key, value in cell_dict.items():
+
+            if len(value) > 1:
+                raise SyntaxError("Waves can't work properly cause there is more then 1 cell in one of dirs", value)
+
+            possible_cells = self.cellsConnections[mat_tmp[cell]]
+            possible_cells = possible_cells[key]
+            if value and mat_tmp[value[0]] in possible_cells:
+                valid_cells.append(value[0])
+
+        return valid_cells, cell
+
+    def get_relative_cells(self,pos, mat = None):
+        #возвращает словарь из четырёх клеток расположенных вокруг какой-то клетки и саму клетку
+        #пример return [{"up":[], "right":[(0,1)], "down":[(1,0)], "left":[]}, (0,0)]
+
+        mat = mat or self._matrix
+
+        y, x = pos
+
+        neighbors = {
+            "up": [(y - 1, x)] if y - 1 >= 0 else [],
+            "down": [(y + 1, x)] if y + 1 < len(mat) else [],
+            "left": [(y, x - 1)] if x - 1 >= 0 else [],
+            "right": [(y, x + 1)] if x + 1 < len(mat[y]) else []
+        }
+        return [neighbors, pos]
+
+    @staticmethod
+    def matrix_max_dimensions(matrix):
+        # возвращает максимальные измерения матрицы переданной в класс в виде
+        # [макс высота, макс ширина]
+
+        return len(matrix), max(len(row) for row in matrix) if len(matrix) > 0 else 0
+
+    def create_wave(self, start_point):
+        y, x = start_point
+        if not (-1 < y < len(self._matrix) - 1 and -1 < x < len(self._matrix[y])):
+            raise ValueError("Out of range start wave point:", start_point, self._matrix)
+
+        waves = [[start_point]]
+        visited = set(waves[0])
+
+        while waves[-1]: #пока в прошлой волне что-то есть
+            next_wave = []
+
+            for element in waves[-1]:
+                for neighbour in self._matrixConnections[element]:
+                    if neighbour not in visited:
+                        next_wave.append(neighbour)
+                        visited.add(neighbour)
+
+            waves.append(next_wave)
+
+        # waves = waves.pop(-1)
+        self.Waves = waves[:-1]
+        # print(waves)
+        return waves
+
+
+
+if __name__ == "__main__":
+    pass
+    # w = WaveCreator(mat)
+    # w.create_wave((0,0))
+
+
