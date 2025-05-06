c16d35ae5ea91943e15fc66ff63875ed9a86f502
EndForced <evafagsclub@gmail.com>
2025-05-06 22:48:51 +1000

    123

---
+++ RaspberriScripts/TryToConnect.py
@@ -0,0 +1,14 @@
+import subprocess
+
+networks = {"Pro":"87658765", "TP-Link_26A0":"96472569"}
+
+res = subprocess.run(["iwgetid","-r"], capture_output = True)
+res = res.stdout.strip()
+
+if res in networks.keys():
+    exit()
+else:
+    pass
+res = subprocess.run(["sudo", "iwlist", "wlan0", "scan", "|", "grep", "ESSID"], capture_output=True)
+res = res.stdout.strip()
+print("possible networks:", res)
