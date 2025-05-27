f0bfc0017c4cc7c29a05a9edb6d7251a4405d838
EndForced <evafagsclub@gmail.com>
2025-05-27 15:52:12 +1000

    123

--- RaspberriScripts/StreamVideo.py
+++ RaspberriScripts/StreamVideo.py
@@ -1,136 +1,6 @@
-# from flask import Flask, render_template
-# from flask_socketio import SocketIO, emit
-# import serial
-# import threading
-# import base64
-# import time
-# import cv2
-# import os
-# class RobotAPI:
-#     def __init__(self, position, orientation):
-#         self.ser = serial.Serial('/dev/ttyAMA0', 115200, timeout = 1)
-#         self.ser.flush()  # очищаем буфер
-#
-#         if max(position) > 15:
-#             raise ValueError("Robot is out of borders!", position)
-#         self.RobotPosition = position
-#
-#         if orientation not in [1,2,3,4]:
-#             raise  ValueError("Unknown orientation!", orientation)
-#         self.RobotOrientation = orientation
-#
-#         self.ESPMessage = self.read()
-#         self.IsDoingAction = 0
-#
-#     def send(self, data):
-#         data_to_send = data + "\n"  # Data to send (must be bytes)
-#         self.ser.write(data_to_send.encode('utf-8'))
-#
-#     def read(self):
-#         line = self.ser.readline().decode('utf-8').strip()
-#
-#         print(line)
-#         if line is not None:
-#             lines = line.split("*")
-#             return lines
-#
-#     def drive_through_roadmap(self, roadmap):
-#         pass
-#
-#     def handle_website_commands(self, args):
-#         self.ser.reset_input_buffer()  # Для pyserial
-#
-#         if args:
-#             res = ""
-#             self.send(args)
-#
-#             while not res:
-#                 res = self.read()
-#
-#             socketio.emit('uart_message', {
-#                 'message': res,
-#                 'type': 'received'
-#             })
-#
-#
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'your_secret_key'
-# socketio = SocketIO(app)
-# robot = RobotAPI((0, 0), 1)
-#
-# camera_configs = {
-#     'low': {'size': (640, 480), 'fps': 30},
-#     'medium': {'size': (1296, 972), 'fps': 20},
-#     'high': {'size': (1920, 1080), 'fps': 15},
-#     'max': {'size': (2592, 1944), 'fps': 10}
-# }
-# current_quality = 'medium'
-# stream_active = False
-#
-# os.environ['LD_LIBRARY_PATH'] = '/usr/lib/aarch64-linux-gnu:' + os.environ.get('LD_LIBRARY_PATH', '')
-# import os
-# import cv2
-# import base64
-# import time
-#
-#
-# def get_frame_from_service():
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#         s.connect(('localhost', 65432))
-#         while True:
-#             # Получаем длину данных
-#             length_bytes = s.recv(4)
-#             if not length_bytes:
-#                 break
-#             length = int.from_bytes(length_bytes, 'big')
-#
-#             # Получаем сами данные
-#             data = b''
-#             while len(data) < length:
-#                 packet = s.recv(length - len(data))
-#                 if not packet:
-#                     break
-#                 data += packet
-#
-#             frame = pickle.loads(data)
-#             yield base64.b64encode(frame).decode('utf-8')
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-# @app.route('/raw_cameras')
-# def get_raw():
-#     robot.handle_website_commands("Reset")
-#     return render_template('raw_cameras.html')
-#
-#
-#
-# @socketio.on('uart_command')
-# def handle_uart_command(data):
-#     command = data.get('command', '')
-#     print(f"Received UART command: {command}")
-#     robot.handle_website_commands(command)
-#
-# @socketio.on('start_stream')
-# def handle_connect():
-#     for frame in get_frame_from_service():
-#         socketio.emit('video_frame', {'data': frame})
-#
-# @socketio.on('stop_stream')
-# def handle_stop_stream():
-#     global stream_active
-#     stream_active = False
-#
-#
-# if __name__ == '__main__':
-#     pass
-#
-# socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
-
-
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+import serial
 import socket
 import pickle
 import threading
@@ -141,34 +11,84 @@ import numpy as np
 import sys
 import os
 import time
+import struct
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from ClientClasses.VisualizationProcessing import VisualizePaths
-
-app = Flask(__name__)
-socketio = SocketIO(app,
-                  async_mode='threading',
-                  engineio_logger=False,
-                  ping_timeout=60,
-                  max_http_buffer_size=50*1024*1024)
-
+# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Инициализация Flask и SocketIO
+app = Flask(__name__)
+socketio = SocketIO(app,
+                   async_mode='threading',
+                   engineio_logger=False,
+                   ping_timeout=60,
+                   max_http_buffer_size=50*1024*1024)
+
+# Конфигурации камер
+camera_configs = {
+    'low': {'size': (640, 480), 'fps': 30},
+    'medium': {'size': (1296, 972), 'fps': 20},
+    'high': {'size': (1920, 1080), 'fps': 15},
+    'max': {'size': (2592, 1944), 'fps': 10}
+}
+current_quality = 'medium'
+stream_active = False
+
+class RobotAPI:
+    def __init__(self, position, orientation):
+        self.ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
+        self.ser.flush()  # очищаем буфер
+
+        if max(position) > 15:
+            raise ValueError("Robot is out of borders!", position)
+        self.RobotPosition = position
+
+        if orientation not in [1, 2, 3, 4]:
+            raise ValueError("Unknown orientation!", orientation)
+        self.RobotOrientation = orientation
+
+        self.ESPMessage = self.read()
+        self.IsDoingAction = 0
+
+    def send(self, data):
+        data_to_send = data + "\n"
+        self.ser.write(data_to_send.encode('utf-8'))
+
+    def read(self):
+        line = self.ser.readline().decode('utf-8').strip()
+        print(line)
+        if line is not None:
+            lines = line.split("*")
+            return lines
+
+    def drive_through_roadmap(self, roadmap):
+        pass
+
+    def handle_website_commands(self, args):
+        self.ser.reset_input_buffer()
+        if args:
+            res = ""
+            self.send(args)
+            while not res:
+                res = self.read()
+            socketio.emit('uart_message', {
+                'message': res,
+                'type': 'received'
+            })
 
 class CameraClient:
     def __init__(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.stream_active = False
         self.lock = threading.Lock()
-        self.map_image = None  # Для хранения изображения карты
+        self.map_image = None
 
     def connect(self):
         try:
             self.client_socket.connect(('localhost', 65432))
             with self.lock:
                 self.stream_active = True
-
 
             self.client_socket.sendall(b"WEBSITE_STREAMING")
             logger.info("Connected to dual camera server")
@@ -191,10 +111,8 @@ class CameraClient:
                         data += packet
 
                     if data:
-                        # Декодируем данные с сервера (словарь с двумя камерами)
                         frames = pickle.loads(data)
 
-                        # Отправляем кадры на клиент с указанием номера камеры
                         if 'camera1' in frames:
                             encoded1 = base64.b64encode(frames['camera1']).decode('utf-8')
                             socketio.emit('video_frame', {
@@ -209,7 +127,6 @@ class CameraClient:
                                 'frame': encoded2
                             })
 
-                        # Отправляем карту если есть (камера 3)
                         if self.map_image is not None:
                             _, buffer = cv2.imencode('.jpg', self.map_image)
                             map_encoded = base64.b64encode(buffer).decode('utf-8')
@@ -232,27 +149,40 @@ class CameraClient:
             with self.lock:
                 self.stream_active = False
 
-
-
+# Инициализация объектов
+robot = RobotAPI((0, 0), 1)
 camera_client = CameraClient()
-
 
 @app.route('/')
 def index():
-    return render_template('raw_cameras.html',
-                           qualities=['low', 'medium', 'high', 'max'])
+    return render_template('index.html')
 
+@app.route('/raw_cameras')
+def get_raw():
+    robot.handle_website_commands("Reset")
+    return render_template('raw_cameras.html',
+                         qualities=['low', 'medium', 'high', 'max'])
+
+@socketio.on('uart_command')
+def handle_uart_command(data):
+    command = data.get('command', '')
+    print(f"Received UART command: {command}")
+    robot.handle_website_commands(command)
 
 @socketio.on('start_stream')
-def handle_start_stream(data):
-    camera = data.get('camera', 1)
-    quality = data.get('quality', 'medium')
+def handle_start_stream(data=None):
+    if data is None:
+        # Режим совместимости со старым кодом
+        camera = 1
+        quality = 'medium'
+    else:
+        camera = data.get('camera', 1)
+        quality = data.get('quality', 'medium')
 
     if camera == 3:
-        # Для камеры 3 (карты) просто активируем поток
         socketio.emit('video_frame', {
             'camera': 3,
-            'frame': ''  # Пустой кадр, если нет изображения
+            'frame': ''
         })
     else:
         with camera_client.lock:
@@ -261,12 +191,15 @@ def handle_start_stream(data):
                 client_thread.daemon = True
                 client_thread.start()
 
-
 @socketio.on('stop_stream')
-def handle_stop_stream(data):
-    camera = data.get('camera', 1)
+def handle_stop_stream(data=None):
+    if data is None:
+        # Режим совместимости со старым кодом
+        camera = 1
+    else:
+        camera = data.get('camera', 1)
+
     if camera == 3:
-        # Для камеры 3 просто отправляем черный кадр
         black_img = np.zeros((480, 640, 3), dtype=np.uint8)
         encoded = base64.b64encode(black_img.tobytes()).decode('utf-8')
         socketio.emit('video_frame', {
@@ -277,9 +210,6 @@ def handle_stop_stream(data):
         with camera_client.lock:
             camera_client.stream_active = False
 
-
 if __name__ == "__main__":
-
     time.sleep(1)
     socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
-
