de255a1891ed25344275f634bd5bba76e009bcd2
EndForced <evafagsclub@gmail.com>
2025-05-29 23:50:26 +1000

    123

--- RaspberriScripts/StreamVideo.py
+++ RaspberriScripts/StreamVideo.py
@@ -17,7 +17,7 @@ class RobotAPI:
 
     def __init__(self, position, orientation, serial, socketio = None):
         self.telemetryQuality = 15
-        self.mapQuality = 32
+        self.mapQuality = 30
 
         self.ser = serial
         self.ser.flush()
