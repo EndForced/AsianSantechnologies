565468b101f85af67e533c80d306a98835fd2098
EndForced <evafagsclub@gmail.com>
2025-05-29 23:26:32 +1000

    123

--- RaspberriScripts/Slam_algorithm.py
+++ RaspberriScripts/Slam_algorithm.py
@@ -30,7 +30,7 @@ class MainComputer(VisualizePaths, WebsiteHolder):
 
     def send_map(self):
         if self.resizedPicture is not None:
-            encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), self.telemetryQuality]
+            encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), self.robot.telemetryQuality]
             buffer1 = cv2.imencode('.jpg', self.resizedPicture, encode_params)
 
             _, buffer = cv2.imencode('.jpg', self.resizedPicture)
@@ -54,6 +54,8 @@ if list(res):
     print("sent")
 
 print(MainComputer.__mro__)
+while 1:
+    pass
 
 
 
