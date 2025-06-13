1918e66c999f22265eac54b7a581c00b9645fc74
EndForced <evafagsclub@gmail.com>
2025-06-13 18:11:33 +1000

    way to commands added

--- RaspberriScripts/Slam_algorithm.py
+++ RaspberriScripts/Slam_algorithm.py
@@ -154,10 +154,11 @@ mc = MainComputer(mat, serial)
 if mc.OS == "Linux":
     # mc.start_website()
     # time.sleep(3)
-    mc.robot.get_uncompressed_frames(1)
+    while 1:
+        mc.robot.get_uncompressed_frames(1)
     #
-    print("Started")
-    exit()
+        print("yep")
+    # exit()
     # p = input()
     mc.qualifiction()
     # time.sleep(1000)
