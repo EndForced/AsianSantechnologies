bfbdd5a58701b6f9a47820b0baa2a38e160078f2
EndForced <evafagsclub@gmail.com>
2025-06-14 13:12:39 +1000

    way to commands added

--- RaspberriScripts/Slam_algorithm.py
+++ RaspberriScripts/Slam_algorithm.py
@@ -152,7 +152,7 @@ mc = MainComputer(mat, serial)
 # print(mc.resizedPicture.dtype)
 
 if mc.OS == "Linux":
-    mc.start_website()
+    # mc.start_website()
     # time.sleep(3)
     while 1:
         mc.robot.get_uncompressed_frames(1)
