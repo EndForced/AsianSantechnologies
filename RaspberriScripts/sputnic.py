57526019ba263447d4f123716047e588ee5f8032
EndForced <evafagsclub@gmail.com>
2025-06-16 17:20:09 +1000

    way to commands added

--- RaspberriScripts/sputnic.py
+++ RaspberriScripts/sputnic.py
@@ -33,7 +33,7 @@ def process_matrix_in_background(mat, serial_conn):
         print("Background processing started")
         mc = MainComputer(mat, serial_conn)
 
-        # mc.robot.do("Beep")
+        mc.robot.do("Beep")
         floor = int(mc.robot.do("MyFloor")[0])
         cord = mc.find_robot()
         mc._matrix[cord[0]][cord[1]] = 71 if floor == 1 else 81
