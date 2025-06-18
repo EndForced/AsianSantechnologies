ee477309bddd591b61a8c75c4c6256bd6c7249a7
EndForced <evafagsclub@gmail.com>
2025-06-18 16:03:08 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/CvProcessing/testing.py
+++ RaspberriScripts/CvProcessing/testing.py
@@ -9,8 +9,8 @@ processed_frame, cells, borders = analyze_frame(frame, floor=1)
 cv2.imshow("tile", processed_frame)
 cv2.waitKey(0)
 
-for key, item in cells.items():
-    if str(item) != "unr":
-        cv2.imshow("tile", item)
-        print(tile_to_code(item))
-        cv2.waitKey(0)
+# for key, item in cells.items():
+#     if str(item) != "unr":
+#         cv2.imshow("tile", item)
+#         print(tile_to_code(item))
+#         cv2.waitKey(0)
