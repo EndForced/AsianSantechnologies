8fea26aff6750b74e5c93dfa153c9637896cfc4a
EndForced <evafagsclub@gmail.com>
2025-06-19 18:34:00 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/CvProcessing/CellDetector.py
+++ RaspberriScripts/CvProcessing/CellDetector.py
@@ -39,6 +39,14 @@ hsw_red = [
 mean_const = 160
 
 
+COLOR_RANGES = {
+    "Red": [np.array([0, 90, 172]), np.array([22, 255, 255]),
+        np.array([150, 100, 80]), np.array([180, 255, 255])],
+    "Green": (np.array([72, 161, 163]), np.array([85, 255, 196])),
+    "Blue": (np.array([110, 90, 90]), np.array([140, 255, 255]))
+}
+
+
 def fix_perspective(img):
     #больше констант??????????
     K = np.array(
@@ -155,15 +163,6 @@ def draw_on_image(img, coordinates, color=(0, 255, 0), thickness=2, fill=False):
     else:
         cv2.polylines(img, [pts], isClosed=True, color=color, thickness=thickness)
     return img
-
-
-COLOR_RANGES = {
-    "Red": [np.array([0, 0, 172]), np.array([22, 255, 255]),
-        np.array([150, 100, 80]), np.array([180, 255, 255])],
-    "Green": (np.array([72, 161, 163]), np.array([85, 255, 196])),
-    "Blue": (np.array([110, 90, 90]), np.array([140, 255, 255]))
-}
-
 
 def search_for_color(frame, color, min_area=50):
     if color not in COLOR_RANGES:
