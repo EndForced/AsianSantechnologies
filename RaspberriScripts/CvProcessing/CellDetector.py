cc553d675394dc1ab27cc2084b5ce2e47152fa43
EndForced <evafagsclub@gmail.com>
2025-06-20 13:46:20 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/CvProcessing/CellDetector.py
+++ RaspberriScripts/CvProcessing/CellDetector.py
@@ -250,11 +250,11 @@ def process_borders(slices, borders, leads, floor):
 
         if floor == 1:
             # ignore  behind white
-            if i == 2 and leads[i] == "black"and tile_to_code(slices[0]) != 31:
+            if i == 2 and leads[0] == "black"and tile_to_code(slices[0]) != 31:
                 ignore_mask[i] = True
                 print("heh")
 
-            if i == 3 and leads[i] == "black"  and tile_to_code(slices[1]) != 31:
+            if i == 3 and leads[1] == "black"  and tile_to_code(slices[1]) != 31:
                 ignore_mask[i] = True
                 print("huh")
         # elif floor == 2:
