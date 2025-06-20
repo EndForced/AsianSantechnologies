f5a76b015504fee3cbc1fff05f0566ae5ebaf30a
EndForced <evafagsclub@gmail.com>
2025-06-20 17:06:48 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/Slam_algorithm.py
+++ RaspberriScripts/Slam_algorithm.py
@@ -266,7 +266,7 @@ class MainComputer(VisualizePaths, WebsiteHolder):
         return result
 
     def move_to_border(self):
-        unrevealed = np.array([[1 if cell == 0 or cell == 99 else 0 for cell in row] for row in self._matrix])
+        unrevealed = np.array([[0 if cell == 0 or cell == 99 else 1 for cell in row] for row in self._matrix])
         print(unrevealed)
 
         interest_mat = self.interest_calculation(unrevealed)
