4eb62a8d533bbd62e54d0c17992e446ab7ec1b57
EndForced <evafagsclub@gmail.com>
2025-06-20 12:57:17 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/SlamLogic/SlamLogic.py
+++ RaspberriScripts/SlamLogic/SlamLogic.py
@@ -13,3 +13,19 @@ def prepare_to_insert(cells, direction):
         print("cells finish")
         return cells
 
+    elif direction == "L":
+        replacements = {41:42, 51:52, 42:41, 52:51, 32:31, 34:33, 31:32, 33:34}#idktbh 61:63, 62:64, 63:61, 64:62
+        for i in cells.keys():
+            if cells[i] in replacements.keys():
+                cells[i] = replacements[i]
+        print("cells finish")
+        return cells
+
+    elif direction == "R": #not sure
+        replacements = {41:42, 51:52, 42:41, 52:51, 32:31, 34:33, 31:32, 33:34}#idktbh 61:63, 62:64, 63:61, 64:62
+        for i in cells.keys():
+            if cells[i] in replacements.keys():
+                cells[i] = replacements[i]
+        print("cells finish")
+        return cells
+
