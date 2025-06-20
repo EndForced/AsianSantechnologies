9dd58089c3505a189f8b0f9f9f6b58d9032f90ed
EndForced <evafagsclub@gmail.com>
2025-06-20 12:50:46 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/SlamLogic/SlamLogic.py
+++ RaspberriScripts/SlamLogic/SlamLogic.py
@@ -2,12 +2,14 @@ from numpy.f2py.symbolic import replace_parenthesis
 
 
 def prepare_to_insert(cells, direction):
+    print("cells start", cells)
     if direction == "U":
         return cells
     elif direction == "D":
         replacements = {31:33, 32:34, 33:31, 34:32, 61:63, 62:64, 63:61, 64:62}
-        for i in range(len(cells)):
+        for i in cells.keys():
             if cells[i] in replacements.keys():
                 cells[i] = replacements[i]
-
+        print("cells finish")
         return cells
+
