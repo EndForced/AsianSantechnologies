39e76900518d4604caff0096102c484d52f59106
EndForced <evafagsclub@gmail.com>
2025-06-26 05:41:49 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/CvProcessing/CellDetector.py
+++ RaspberriScripts/CvProcessing/CellDetector.py
@@ -2,7 +2,7 @@ import os
 import sys
 from itertools import combinations
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(file))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(file))))
 import cv2
 import numpy as np
 
