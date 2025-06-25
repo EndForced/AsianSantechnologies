ec653a1c638e299a5e8e50349001935c25d19e69
EndForced <evafagsclub@gmail.com>
2025-06-26 05:53:45 +1000

    Merge remote-tracking branch 'origin/main'

--- RaspberriScripts/CvProcessing/CellDetector.py
+++ RaspberriScripts/CvProcessing/CellDetector.py
@@ -278,17 +278,20 @@ def process_borders(slices, borders, leads, floor):
 
 
 def shuffle_slices(slices, slices2):
-    all_slices  = {i: np.array([1488]) for i in range(12)}  # Инициализируем маску
+    all_slices = {}
 
     all_slices[0] = slices2[0]
-    all_slices[1:2] = slices[0:1]
+    all_slices[1] = slices[0]
+    all_slices[2] = slices[1]
     all_slices[3] = slices2[1]
-    all_slices[4:5] = slices[2:3]
-
+    all_slices[4] = slices[2]
+    all_slices[5] = slices[3]
     all_slices[6] = slices2[2]
-    all_slices[7:8] = slices[4:5]
+    all_slices[7] = slices[4]
+    all_slices[8] = slices[5]
     all_slices[9] = slices2[3]
-    all_slices[10:11] = slices[6:7]
+    all_slices[10] = slices[6]
+    all_slices[11] = slices[7]
 
     return all_slices
 
